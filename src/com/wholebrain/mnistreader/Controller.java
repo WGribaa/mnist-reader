d2639b6e1f5aabbf18be44a922042a4e8b660561
Wael Gribaa <g.wael@outlook.fr>
2019-08-02 18:02:56 +0200

    Added showing of labels associated with images, with mapping textfiles(for emnist) or defaultmapping (for mnist)

--- src/com/wholebrain/mnistreader/Controller.java
+++ src/com/wholebrain/mnistreader/Controller.java
@@ -9,9 +9,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.CheckMenuItem;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioMenuItem;
 import javafx.scene.control.ScrollBar;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
@@ -22,28 +25,39 @@ import javafx.stage.Stage;
 
 import java.awt.Desktop;
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 
 public class Controller implements Initializable {
     public MenuItem open_menu, close_menu;
     public Canvas canvas;
-    public Label index_label;
+    public Label index_label, char_label;
     public ScrollBar index_scrollbar;
     public ColorPicker empty_color_picker, full_color_picker;
     public TextField jumpto_textfield;
+    public CheckMenuItem labels_checkbox;
+    public Menu labelposition_menu;
+    public RadioMenuItem topleft_position_radiomenu,topright_position_radiomenu,
+            bottomleft_position_radiomenu,bottomright_position_radiomenu,top_position_radiomenu,
+            bottom_position_radiomenu,left_position_radiomenu,right_position_radiomenu;
 
     private Stage primaryStage;
     private File currentFile;
     private int magicNumber, numberOfImages, numberOfRows, numberOfColumns, currentImageIndex = 0;
     private Color[] pallet = new Color[256];
     private boolean needsTransformation = false;
+    private char[] labelsChars;
+    private Map<Integer, Character> mapping;
 
     /**
      * Closes the application.
@@ -81,7 +95,6 @@ public class Controller implements Initializable {
     public void emnist_goto() {
         gotoHtmlLink("https://www.nist.gov/node/1298471/emnist-dataset");
     }
-
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -126,6 +139,48 @@ public class Controller implements Initializable {
             updateIndex(newInt);
         });
 
+        char_label.visibleProperty().bind(labels_checkbox.selectedProperty());
+
+        initializeMenus();
+
+    }
+
+    private void initializeMenus(){
+        labelposition_menu.disableProperty().bind(labels_checkbox.disableProperty());
+
+        topleft_position_radiomenu.setOnAction(event -> {
+            char_label.setLayoutX(0);
+            char_label.setLayoutY(0);
+        });
+        topright_position_radiomenu.setOnAction(event -> {
+            char_label.setLayoutX(canvas.getWidth()-char_label.getWidth());
+            char_label.setLayoutY(0);
+        });
+        bottomleft_position_radiomenu.setOnAction(event -> {
+            char_label.setLayoutX(0);
+            char_label.setLayoutY(canvas.getHeight()-char_label.getHeight());
+        });
+        bottomright_position_radiomenu.setOnAction(event -> {
+            char_label.setLayoutX(canvas.getWidth()-char_label.getWidth());
+            char_label.setLayoutY(canvas.getHeight()-char_label.getHeight());
+        });
+        top_position_radiomenu.setOnAction(event -> {
+            char_label.setLayoutX((canvas.getWidth()-char_label.getWidth())/2);
+            char_label.setLayoutY(0);
+        });
+        bottom_position_radiomenu.setOnAction(event -> {
+            char_label.setLayoutX((canvas.getWidth()-char_label.getWidth())/2);
+            char_label.setLayoutY(canvas.getHeight()-char_label.getHeight());
+        });
+        left_position_radiomenu.setOnAction(event -> {
+            char_label.setLayoutX(0);
+            char_label.setLayoutY((canvas.getHeight()-char_label.getHeight())/2);
+        });
+        right_position_radiomenu.setOnAction(event -> {
+            char_label.setLayoutX(canvas.getWidth()-char_label.getWidth());
+            char_label.setLayoutY((canvas.getHeight()-char_label.getHeight())/2);
+        });
+
     }
 
     /**
@@ -146,7 +201,7 @@ public class Controller implements Initializable {
         jumpto_textfield.setText(String.valueOf(newIndex));
         index_scrollbar.setValue(newIndex);
         // Corrects the orientation of the imageBuffer if it comes from an EMNIST dataset.
-        needsTransformation = currentFile.getName().contains("-idx3-ubyte");
+        needsTransformation = currentFile.getName().contains("emnist");
         paint();
     }
 
@@ -179,9 +234,111 @@ public class Controller implements Initializable {
                     e.printStackTrace();
                 }
         }
+        loadLabels();
         index_scrollbar.setMax(numberOfImages-1);
         index_scrollbar.setBlockIncrement(numberOfImages/20);
         updateIndex(0);
+    }
+
+    /**
+     * Reads the labels file associated with the current image file and save it in a byte array.
+     * If the number of images is different from the number of labels, the byte array will remain null.
+     */
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    private void loadLabels() {
+        labelsChars = null;
+
+        StringBuilder labelSb = new StringBuilder(currentFile.getPath());
+        labelSb.replace(labelSb.lastIndexOf("idx3"),labelSb.lastIndexOf("idx3")+4,"idx1")
+                .replace(labelSb.lastIndexOf("images"), labelSb.lastIndexOf("images")+6, "labels");
+        File labelFile = new File(labelSb.toString());
+        if (!labelFile.exists())
+            return;
+
+        BufferedInputStream bis = null;
+        byte[] labelsBytes = null;
+        try{
+            bis = new BufferedInputStream((new FileInputStream(labelFile)));
+            bis.skip(4);
+            byte[] buffer = new byte[4];
+            bis.read(buffer);
+            if(bytesToInt(buffer) != numberOfImages)
+                return;
+            labelsBytes = bis.readAllBytes();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (bis != null)
+                    bis.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (labelsBytes==null || labelsBytes.length != numberOfImages)
+            labelsChars = null;
+        else {
+            loadMapping();
+            labelsChars = new char[labelsBytes.length];
+            for(int i =0; i< labelsBytes.length; i++)
+                labelsChars[i]= mapping.get((int) labelsBytes[i]);
+
+            labels_checkbox.setDisable(false);
+            labels_checkbox.setSelected(true);
+        }
+    }
+
+    private void loadMapping(){
+        mapping = new HashMap<>();
+        if(currentFile.getPath().lastIndexOf("emnist")==-1) {
+            System.out.println("No loading because not an Emnist dataset.");
+            defaultMapping();
+            return;
+        }
+        String mappingType = currentFile.getName().split("-")[1];
+        File mappingFile = new File(currentFile.getParent()+File.separator+"emnist-"+mappingType+"-mapping.txt");
+        if (!mappingFile.exists()) {
+            System.out.println("No loading because the file \""+ mappingFile.getPath()+"\" doesn't exist.");
+            defaultMapping();
+            return;
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(mappingFile))){
+            String line = br.readLine();
+            String[] splittedLine;
+            while(line!=null){
+                splittedLine = line.split(" ");
+                mapping.put(Integer.valueOf(splittedLine[0]),(char)Integer.parseInt(splittedLine[1]));
+                line=br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Mapping file \""+mappingFile.getPath()+"\" has been loaded.");
+        System.out.println(mapping);
+
+    }
+
+    private void defaultMapping(){
+        for (int i = 0; i<10;i++){
+            mapping.put(i,(char)(i+48));
+        }
+        System.out.println("Default mapping has been loaded.");
+        System.out.println(mapping);
+    }
+
+
+    /**
+     * Shows the label associated with the current image.
+     */
+    private void updateLabel(){
+        if(labelsChars ==null) {
+            char_label.setText(null);
+        } else{
+//            System.out.println(labelsChars[currentImageIndex]);
+            char_label.setText(String.valueOf(labelsChars[currentImageIndex]));
+        }
     }
 
     /**
@@ -216,6 +373,7 @@ public class Controller implements Initializable {
                 gc.setFill(pallet[imageBuffer[y*numberOfRows+x]&0xFF]);
                 gc.fillRect(x*resolution, y*resolution,resolution, resolution);
             }
+        updateLabel();
     }
 
     /**
